8ea439cf9735f239a3f58b9736921795c26f9162
Suresh Mekarthi <sureshmekarthi@gmail.com>
2026-02-03 17:49:33 -0500

    print 5 to 1

--- Recurssion/Program.cs
+++ Recurssion/Program.cs
@@ -10,6 +10,20 @@ namespace Recurssion
     {
         static void Main(string[] args)
         {
+            //print numbers from 5 to 1
+            int n = 5;
+            Print(n);
+            Console.ReadLine();
+        }
+
+        static void Print(int n) 
+        { 
+            if((n<=0))
+            {
+                return;
+            }
+            Console.WriteLine(n);
+            Print(n - 1);
         }
     }
 }
