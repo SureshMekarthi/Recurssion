937351611e8f20f3f183bb6c27f017b11f80c370
Suresh Mekarthi <sureshmekarthi@gmail.com>
2026-02-04 11:01:26 -0500

    n fac and sun

--- Recurssion/Program.cs
+++ Recurssion/Program.cs
@@ -12,7 +12,10 @@ namespace Recurssion
         {
             //print numbers from 5 to 1
             int n = 5;
-            Print(n);
+            // Print(n);
+           int resul = Fac(n);
+            Console.WriteLine(resul);
+
             Console.ReadLine();
         }
 
@@ -24,6 +27,30 @@ namespace Recurssion
             }
             Console.WriteLine(n);
             Print(n - 1);
+        }
+        static void Print1(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            Print1(n - 1);
+            Console.WriteLine(n);
+        }
+
+        static int Sum(int n) {
+            if (n == 0) 
+            { return 0; }
+            return n+Sum(n-1);
+        }
+
+        static int Fac(int n)
+        {
+            if (n == 1 || n == 0)
+                return 1;
+
+            return n * Fac(n - 1);
         }
     }
 }
